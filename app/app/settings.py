5538e66b6f4df689349439d37f7f9eb6cf5a93e3
Philip Crockett <philip.crockett@thermondo.de>
2023-02-07 17:42:13 +0100

    Use Postgres instead of SQLite

--- app/app/settings.py
+++ app/app/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': 'db',
+        'PORT': '5432',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
     }
 }
 
